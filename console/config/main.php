df877d54255748a42652731453e36d543c336bd4
Ziyat <termit_90@mail.ru>
2019-02-25 22:14:47 +0500

    fixed

--- console/config/main.php
+++ console/config/main.php
@@ -4,7 +4,6 @@ $params = array_merge(
     require __DIR__ . '/../../common/config/params-local.php',
     require __DIR__ . '/params.php',
     require __DIR__ . '/params-local.php'
-
 );
 
 return [
