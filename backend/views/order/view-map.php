e0617d4f68f192aaddb64a4277165e4082189d47
Ziyat <termit_90@mail.ru>
2019-04-10 14:44:06 +0500

    fixed

--- backend/views/order/view-map.php
+++ backend/views/order/view-map.php
@@ -6,61 +6,10 @@
  * @var $order \uztelecom\entities\orders\Order
  */
 
-use dosamigos\leaflet\layers\Marker;
-use dosamigos\leaflet\layers\TileLayer;
-use dosamigos\leaflet\LeafLet;
-use dosamigos\leaflet\types\Icon;
-use dosamigos\leaflet\types\LatLng;
-use dosamigos\leaflet\types\LatLngBounds;
-use dosamigos\leaflet\types\Point;
-use dosamigos\leaflet\widgets\Map;
-
+use uztelecom\helpers\OrderHelper;
 
 $this->title = 'Маршрут на карте';
 $this->params['breadcrumbs'][] = ['label' => 'Заказы', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
-$center = new LatLng(['lat' => $order->from_lat, 'lng' => $order->from_lng]);
-$marker = new Marker(['latLng' => $center, 'popupContent' => $order->from_address]);
-$icon = new Icon(['iconUrl' => '/img/dotA.png']);
-$icon->setIconSize(new Point(['x' => 31, 'y' => 40]));
-$icon->setIconAnchor(new Point(['x' => 16, 'y' => 5]));
-$marker->setIcon($icon);
-$markers[] = $marker;
-
-if ($order->to_lat && $order->to_lng) {
-    $center = new LatLng(['lat' => $order->to_lat, 'lng' => $order->to_lng]);
-    $marker = new Marker(['latLng' => $center, 'popupContent' => $order->to_address]);
-    $icon = new Icon(['iconUrl' => '/img/dotB.png']);
-    $icon->setIconSize(new Point(['x' => 41, 'y' => 52]));
-    $icon->setIconAnchor(new Point(['x' => 16, 'y' => 5]));
-    $marker->setIcon($icon);
-    $markers[] = $marker;
-    $bounds = new LatLngBounds([
-        'southWest' => new LatLng(['lat' => $order->from_lat, 'lng' => $order->from_lng]),
-        'northEast' => new LatLng(['lat' => $order->to_lat, 'lng' => $order->to_lng])
-    ]);
-}
-
-$tileLayer = new TileLayer([
-    'urlTemplate' => 'https://map.uztelecom.uz/hot/{z}/{x}/{y}.png',
-]);
-
-$leaflet = new LeafLet([
-    'center' => $center,
-    'clientOptions' => [
-        'bounds' => !empty($bounds) ? $bounds->encode() : null
-    ],
-]);
-
-foreach ($markers as $marker) {
-    $leaflet->addLayer($marker);
-}
-$leaflet->addLayer($tileLayer);
-
-try {
-    echo Map::widget(['leafLet' => $leaflet, 'height' => '550px']);
-} catch (\Exception $e) {
-    echo $e->getMessage();
-}
-
+echo OrderHelper::getMap($order);
--- uztelecom/helpers/OrderHelper.php
+++ uztelecom/helpers/OrderHelper.php
@@ -7,6 +7,14 @@
 namespace uztelecom\helpers;
 
 
+use dosamigos\leaflet\layers\Marker;
+use dosamigos\leaflet\layers\TileLayer;
+use dosamigos\leaflet\LeafLet;
+use dosamigos\leaflet\types\Icon;
+use dosamigos\leaflet\types\LatLng;
+use dosamigos\leaflet\types\LatLngBounds;
+use dosamigos\leaflet\types\Point;
+use dosamigos\leaflet\widgets\Map;
 use uztelecom\entities\orders\Order;
 use yii\helpers\Html;
 
@@ -47,6 +55,7 @@ class OrderHelper
         }
         return Html::tag('span',self::getStatusList()[$status] ?? 'Неизвестен',['class' => "label label-$class"]);
     }
+
     public static function serializeStatus($status)
     {
         $name = null;
@@ -76,5 +85,57 @@ class OrderHelper
             'name' => $name,
             'code' => $code,
         ];
+    }
+
+    /**
+     * @param Order $order
+     * @return string
+     */
+    public static function getMap(Order $order)
+    {
+
+        $center = new LatLng(['lat' => $order->from_lat, 'lng' => $order->from_lng]);
+        $marker = new Marker(['latLng' => $center, 'popupContent' => $order->from_address]);
+        $icon = new Icon(['iconUrl' => '/img/dotA.png']);
+        $icon->setIconSize(new Point(['x' => 31, 'y' => 40]));
+        $icon->setIconAnchor(new Point(['x' => 16, 'y' => 5]));
+        $marker->setIcon($icon);
+        $markers[] = $marker;
+
+        if ($order->to_lat && $order->to_lng) {
+            $center = new LatLng(['lat' => $order->to_lat, 'lng' => $order->to_lng]);
+            $marker = new Marker(['latLng' => $center, 'popupContent' => $order->to_address]);
+            $icon = new Icon(['iconUrl' => '/img/dotB.png']);
+            $icon->setIconSize(new Point(['x' => 41, 'y' => 52]));
+            $icon->setIconAnchor(new Point(['x' => 16, 'y' => 5]));
+            $marker->setIcon($icon);
+            $markers[] = $marker;
+            $bounds = new LatLngBounds([
+                'southWest' => new LatLng(['lat' => $order->from_lat, 'lng' => $order->from_lng]),
+                'northEast' => new LatLng(['lat' => $order->to_lat, 'lng' => $order->to_lng])
+            ]);
+        }
+
+        $tileLayer = new TileLayer([
+            'urlTemplate' => 'https://map.uztelecom.uz/hot/{z}/{x}/{y}.png',
+        ]);
+
+        $leaflet = new LeafLet([
+            'center' => $center,
+            'clientOptions' => [
+                'bounds' => !empty($bounds) ? $bounds->encode() : null
+            ],
+        ]);
+
+        foreach ($markers as $marker) {
+            $leaflet->addLayer($marker);
+        }
+        $leaflet->addLayer($tileLayer);
+        try {
+            $result = Map::widget(['leafLet' => $leaflet, 'height' => '550px']);
+        } catch (\Exception $e) {
+            $result = $e->getMessage();
+        }
+        return $result;
     }
 }
