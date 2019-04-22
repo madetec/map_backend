90e29ee931a145e2fc624bfed26e1497b98bb42e
Ziyat <termit_90@mail.ru>
2019-04-23 00:03:36 +0500

    fixed

--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -13,7 +13,6 @@ if (!YII_ENV_TEST) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
-
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
