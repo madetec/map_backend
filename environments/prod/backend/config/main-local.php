ecfc106e8df0620a000a8b52ff15453a4a271790
Ziyat <termit_90@mail.ru>
2019-04-23 00:06:37 +0500

    fixed

--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -1,22 +1,8 @@
 <?php
-$config =  [
+return [
     'components' => [
         'request' => [
             'cookieValidationKey' => '6gzybhetJhQBEj45tEDLcm2n5hnZaCNb3dfTC3rqMmsBEFez',
         ],
     ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
-return $config;
