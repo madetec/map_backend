b76a7429fb8f7ac7737698c4b1c3f21770c610f4
Ziyat <termit_90@mail.ru>
2019-04-22 23:41:12 +0500

    fixe

--- backend/forms/OrderSearch.php
+++ backend/forms/OrderSearch.php
@@ -32,7 +32,7 @@ class OrderSearch extends Model
      */
     public function search(array $params): ActiveDataProvider
     {
-        $query = Order::find()->alias('o')->orderBy(['created_at' => SORT_DESC]);
+        $query = Order::find()->orderBy(['created_at' => SORT_DESC])->alias('o');
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
