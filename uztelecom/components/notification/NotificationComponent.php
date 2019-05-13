7b0e7e7548cb8c5e1a8742480e07ca2bfa112b20
Ziyat <termit_90@mail.ru>
2019-05-13 13:01:55 +0500

    fixed

--- uztelecom/components/notification/NotificationComponent.php
+++ uztelecom/components/notification/NotificationComponent.php
@@ -6,32 +6,52 @@
 
 namespace uztelecom\components\notification;
 
+use paragraph1\phpFCM\Recipient\Device;
+use understeam\fcm\Client;
 use uztelecom\entities\notification\Notification;
+use uztelecom\entities\user\User;
 use uztelecom\events\notification\NotificationEvent;
 use uztelecom\repositories\NotificationRepository;
+use uztelecom\repositories\OrderRepository;
 use uztelecom\repositories\UserRepository;
+use Yii;
 use yii\base\Component;
 
+/**
+ * Class NotificationComponent
+ * @package uztelecom\components\notification
+ * @property Client $fcm
+ * @property UserRepository $users
+ * @property NotificationRepository $notifications
+ * @property OrderRepository $orders
+ */
 class NotificationComponent extends Component
 {
-    public $notifications;
-    public $users;
+    private $notifications;
+    private $users;
+    private $fcm;
 
     public function __construct(
         UserRepository $userRepository,
         NotificationRepository $notificationRepository,
+        OrderRepository $orderRepository,
         array $config = []
     )
     {
         $this->users = $userRepository;
         $this->notifications = $notificationRepository;
+        $this->orders = $orderRepository;
+        $this->fcm = Yii::$app->firebase;
         parent::__construct($config);
     }
 
     /**
      * @param NotificationEvent $event
      * @throws \DomainException
+     * @throws \InvalidArgumentException
+     * @throws \UnexpectedValueException
      * @throws \uztelecom\exceptions\NotFoundException
+     * @throws \yii\base\InvalidParamException
      */
     public function newOrder(NotificationEvent $event)
     {
@@ -41,10 +61,49 @@ class NotificationComponent extends Component
             $event->type_id,
             $event->from_id
         );
+        $order = $this->orders->find($event->type_id);
+
+        /** @var User[] $drivers */
         foreach ($drivers as $driver) {
             $notification->assign($driver->id);
+            foreach ($driver->devices as $device) {
+                $this->sendPushNotificationToDevice(
+                    'Новый заказ',
+                    "От {$order->from_address} до {$order->to_address}\n Заказчик: {$order->user->profile->getFullName()}",
+                    $device->firebase_token
+                );
+            }
         }
         $this->notifications->save($notification);
 
+    }
+
+    /**
+     * @param string $title
+     * @param string $body
+     * @param string $firebase_token
+     * @param array|null $data
+     * @throws \InvalidArgumentException
+     * @throws \UnexpectedValueException
+     * @throws \yii\base\InvalidParamException
+     */
+    private function sendPushNotificationToDevice(string $title, string $body, string $firebase_token, array $data = null)
+    {
+        $note = $this->fcm->createNotification($title, $body);
+
+        $note->setIcon('icon_round.png')
+            ->setColor('#ffffff')
+            ->setBadge(1)
+            ->setSound('default');
+
+        $message = $this->fcm->createMessage();
+
+        if ($data) {
+            $message->setNotification($note)->setData($data);
+        }
+
+        $message->addRecipient(new Device($firebase_token));
+        $response = $this->fcm->send($message);
+        $response->getStatusCode();
     }
 }
