7b49c7c4f8563f29e1073c52db32850645cc6bf2
Ziyat <termit_90@mail.ru>
2019-06-06 15:19:35 +0500

    fixed

--- backend/views/subdivision/index.php
+++ backend/views/subdivision/index.php
@@ -10,32 +10,36 @@ use yii\helpers\Html;
 $this->title = 'Филиалы';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="subdivision-index">
+<div class="box">
+    <div class="box-header">
     <p>
         <?= Html::a('Добавить филиал', ['create'], ['class' => 'btn btn-telecom-car btn-success']) ?>
     </p>
-    <div class="table-responsive">
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            [
-                'attribute' => 'name',
-                'value' => function (\uztelecom\entities\Subdivision $subdivision) {
-                    return Html::a($subdivision->name, \yii\helpers\Url::to(['subdivision/view', 'id' => $subdivision->id]));
-                },
-                'format' => 'raw',
-                'contentOptions' => ['style' => 'white-space: normal;'],
-            ],
-            'lat',
-            'lng',
-            [
-                'attribute' => 'address',
-                'contentOptions' => ['style' => 'white-space: normal;'],
-            ],
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+        <div class="box-body">
+            <div class="table-responsive">
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'filterModel' => $searchModel,
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        [
+                            'attribute' => 'name',
+                            'value' => function (\uztelecom\entities\Subdivision $subdivision) {
+                                return Html::a($subdivision->name, \yii\helpers\Url::to(['subdivision/view', 'id' => $subdivision->id]));
+                            },
+                            'format' => 'raw',
+                            'contentOptions' => ['style' => 'white-space: normal;'],
+                        ],
+                        'lat',
+                        'lng',
+                        [
+                            'attribute' => 'address',
+                            'contentOptions' => ['style' => 'white-space: normal;'],
+                        ],
+                        ['class' => 'yii\grid\ActionColumn'],
+                    ],
+                ]); ?>
+            </div>
+        </div>
     </div>
 </div>
