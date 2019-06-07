a20b200a716233bcd64697e401bc479f490fbe35
Ziyat <termit_90@mail.ru>
2019-06-07 18:05:20 +0500

    fixed

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -21,7 +21,7 @@ $this->title = 'Панель управления, TelecomCar';
         </div>
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
-                <a href="<?= \yii\helpers\Url::to('/order/index') ?>">
+                <a href="<?= \yii\helpers\Url::to('/user/index') ?>">
                     <span class="info-box-icon bg-aqua"><i class="ion-model-s"></i></span>
                     <div class="info-box-content">
                         <span class="info-box-text text-black">Автомобилей</span>
