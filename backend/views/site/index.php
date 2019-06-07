7c02bfe00c595610ac0220243cdbb0a9fe9b5d32
Ziyat <termit_90@mail.ru>
2019-06-07 18:21:18 +0500

    fixed

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -32,7 +32,7 @@ $this->title = 'Панель управления, TelecomCar';
         </div>
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
-                <a href="<?= \yii\helpers\Url::to('/user/index') ?>">
+                <a href="<?= \yii\helpers\Url::to(['/user/index','UserSearch[role]' => 'driver']) ?>">
                     <span class="info-box-icon bg-blue"><i class="ion-ios-people"></i></span>
                     <div class="info-box-content">
                         <span class="info-box-text text-black">Водителей</span>
