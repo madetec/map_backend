e2f0593d046cbc43298be32255df7b59630e9490
Ziyat <termit_90@mail.ru>
2019-06-18 01:31:09 +0500

    fixed

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,7 @@
 return [
     'name' => 'Telecom Car',
     'language' => 'ru',
+    'timeZone' => 'Asia/Tashkent',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
