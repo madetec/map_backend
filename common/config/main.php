8260d72de6483fb2015953977f8b550065957836
Ziyat <termit_90@mail.ru>
2019-06-18 01:27:24 +0500

    fixed

--- common/config/main.php
+++ common/config/main.php
@@ -8,6 +8,10 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'formatter' => [
+            'class'           => 'yii\i18n\Formatter',
+            'defaultTimeZone' => 'Asia/Tashkent',
+        ],
         'firebase' => [
             'class' => 'understeam\fcm\Client',
             'apiKey' => 'AAAAeBjY4rQ:APA91bEzg1-zcVNrXoD2FJRNMjBvPzAYdwX1s6sgFCXHw0HuY_ZzxDGlehAYJwbY_LvdbOwrteM7OgdW7FXRdllNCghs3bArNlLsF5Ld2pYGcWMcvQJWzdvodTx5ML0XzD02P6QjTifB',
